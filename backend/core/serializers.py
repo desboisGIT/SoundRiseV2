d1029df137c0147a1b6431c81a90d04d69b1194d
ValentinLallement <lallement.valentin.lv@gmail.com>
2025-03-08 20:02:48 +0100

    user's beats, followers and following

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,21 +1,30 @@
 from rest_framework import serializers
 from .models import CustomUser,Report,Notifications
+from beats.models import Beat
 import urllib.parse
 
 class CustomUserSerializer(serializers.ModelSerializer):
     profile_picture = serializers.SerializerMethodField()
+    beats = serializers.SerializerMethodField()  # Ajout des beats liés à l'utilisateur
 
     class Meta:
         model = CustomUser
-        fields = ["id", "username", "email", "profile_picture", "bio","badges","trending_beats","recent_publications","instagram","youtube","spotify","twitter","soundcloud","tiktok","apple_music","website", "is_online"]
+        fields = [
+            "id", "username", "email", "profile_picture", "bio", "badges",
+            "trending_beats", "recent_publications", "instagram", "youtube",
+            "spotify", "twitter", "soundcloud", "tiktok", "apple_music",
+            "website", "is_online", "beats","following","followers"  # Ajout du champ beats
+        ]
 
     def get_profile_picture(self, obj):
         request = self.context.get("request")
         profile_picture_url = obj.profile_picture.url if obj.profile_picture else ""
-
-       
-
         return profile_picture_url
+    
+    def get_beats(self, obj):
+        """Retourne les beats liés à cet utilisateur en tant que main_artist"""
+        beats = Beat.objects.filter(main_artist=obj)  # Filtre les beats où l'utilisateur est l'artiste principal
+        return [{"id": beat.id, "title": beat.title, "created_at": beat.created_at} for beat in beats]
 
 class ProfilePictureSerializer(serializers.ModelSerializer):
     class Meta:
